9053a37390a1105b94f10cf2d3357d85d2ed2a56
SeregaC <77716512serega@gmail.com>
2022-05-11 01:12:29 +0300

    a

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception, IllegalArgumentException {
         Calculator calculator = new Calculator();
         String input = null;
         Calculator.calc(input);
@@ -15,14 +15,16 @@ class Main {
         static String operation;
         static Rome n1;
         static Rome n2;
+        static int num1;
+        static int num2;
 
 
-        public static String calc(String input) throws Exception {
+        public static String calc(String input) throws Exception,IllegalArgumentException {
             System.out.println("Input");
             Scanner sc = new Scanner(System.in);
             String str = sc.nextLine();
             String[] mas = str.split(" ");
-            if( mas.length !=3) throw new Exception("Неверный формат ввода. ВВедите через пробел");
+            if (mas.length != 3) throw new Exception("Неверный формат ввода. ВВедите через пробел");
             Rome[] r1 = Rome.values();
             boolean flag = false;
             for (Rome el : r1) {
@@ -32,83 +34,93 @@ class Main {
             }
 
             if (flag) {
-               try { n1 = Rome.valueOf(mas[0]);
-                     n2 = Rome.valueOf(mas[2]);
-                     operation = mas[1];
-                     romeAction(n1, n2, operation);}
-               catch  (IllegalArgumentException e)
-                   { System.out.println("Неверный формат чисел");}
+                try {
+                    n1 = Rome.valueOf(mas[0]);
+                    n2 = Rome.valueOf(mas[2]);
+                    operation = mas[1];
+                    num1 = n1.getValue();
+                    num2 = n2.getValue();
+                    romeAction(n1, n2, operation);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Неверный формат чисел");
+                }
             } else {
-               try{ number1 = Integer.parseInt(mas[0]);
+                try {
+                    number1 = Integer.parseInt(mas[0]);
                     operation = mas[1];
                     number2 = Integer.parseInt(mas[2]);
-                    arabAction();}
-               catch (NumberFormatException e){
-                   System.out.println("Неверный формат числел");
-               }
+                    arabAction();
+                } catch (NumberFormatException e) {
+                    System.out.println("Неверный формат числел");
+                }
             }
             return input;
         }
 
-            static void arabAction () throws Exception{
-                System.out.println("Output");
-            if (number1 > 0 && number1 <= 10 && number2 > 0 && number2 <= 10)
-            throw new Exception("Числа вне диапазона");
-            {
+        static void arabAction() throws Exception,IllegalArgumentException {
+            System.out.println("Output");
+            if (number1 > 0 && number1 <= 10 && number2 > 0 && number2 <= 10) {
 
-                    switch (operation) {
-                        case "-":
-                            System.out.println(number1 - number2);
-                            break;
-                        case "+":
-                            System.out.println(number1 + number2);
-                            break;
-                        case "/":
-                            System.out.println(number1 / number2);
-                            break;
-                        case "*":
-                            System.out.println(number1 * number2);
-                            break;
-                        default:
-                            throw new Exception("Нет такой операции");
+                switch (operation) {
+                    case "-":
+                        System.out.println(number1 - number2);
+                        break;
+                    case "+":
+                        System.out.println(number1 + number2);
+                        break;
+                    case "/":
+                        System.out.println(number1 / number2);
+                        break;
+                    case "*":
+                        System.out.println(number1 * number2);
+                        break;
+                    default:
+                        throw new Exception("Нет такой операции");
+                }
+            } else {
+                throw new Exception("Числа вне диапазона");
+            }
+        }
+
+        static void romeAction(Rome n1, Rome n2, String operation) throws Exception,IllegalArgumentException {
+            System.out.println("Output");
+            int result = 0;
+            if (num1 > 0 && num1 <= 10 && num2 > 0 && num2 <= 10) {
+
+                switch (operation) {
+                    case "+":
+                        result = num1 + num2;
+
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        if (result <= 0) {
+                            throw new Exception("Нет отрицательных римских чисел");
+                        }
+                        break;
+
+                    case "/":
+                        result = num1 / num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + operation);
+                }
+
+                Rome[] rome = Rome.values();
+                for (Rome rome1 : rome) {
+                    if (rome1.value == result) {
+                        System.out.println(rome1);
                     }
                 }
-            }
-
-
-        static void romeAction(Rome n1, Rome n2, String operation) throws Exception {
-            System.out.println("Output");
-        int result = 0;
-
-            switch (operation) {
-                case "+":
-                    result = n1.getValue() + n2.getValue();
-
-                    break;
-                case "-":
-                    result = n1.getValue() - n2.getValue();
-                     if(result<=0); throw new Exception ("Нет отрицательных римских чисел");
-
-                case "/":
-                    result = n1.getValue() / n2.getValue();
-                    break;
-                case "*":
-                    result = n1.getValue() * n2.getValue();
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + operation);
-            }
-
-            Rome[] rome = Rome.values();
-            for (Rome rome1 : rome) {
-                if (rome1.value == result) {
-                    System.out.println(rome1);
-                }
+            } else {
+                throw new Exception("Числа вне диапазона");
             }
         }
     }
-    }
-
+}
 
 
 
